f847e30b70b14efb9794bc22e19c11dfacec2122
Sreejan Chattopadhyay <chattopadhyaysreejan100@gmail.com>
2025-04-10 23:03:18 +0530

    Created Login Page

---
+++ src/app/Login.js
@@ -0,0 +1,26 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import "./Login.css";
+
+
+export default function Login() {
+
+ return (
+
+    <html lang="en">
+        <body>
+            <div className="login-container">
+
+                
+            </div>
+
+        </body>
+
+
+    </html>
+
+ );
+
+
+}
