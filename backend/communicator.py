18d139539774a3efbf55d70fb113ad9bd235d595
Sreejan Chattopadhyay <chattopadhyaysreejan100@gmail.com>
2025-07-20 23:14:01 +0530

    Update communicator.py
    added logic for login and registration for android application

--- backend/communicator.py
+++ backend/communicator.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify,request 
 from flask_cors import CORS
+import mysql.connector
+import bcrypt
 
 
 
 app = Flask(__name__)
 CORS(app)
+
+connection=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password = "Debjanilover09",
+    database="travelinvestgpt"
+    )
 
 
 
@@ -21,13 +30,48 @@ def receive_user_input():
 
 @app.route('/android_register', methods=['POST'])
 def android_register():
-    return "Hi"
+    print('Registration Request from android app received')
+    connection.autocommit = False
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+    cursor = connection.cursor()
+    cursor.execute("Select * from User  where name = '{0}'".format(username))
+    results1 = cursor.fetchone()
+    if results1:
+        return jsonify({'message': 'User already exists'}), 400
+
+    insertsql="Insert into User (name, email, password) values (%s,%s,%s)"
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password,salt)
+    data= (username,email,hashed_password)
+    cursor.execute(insertsql, data)
+    connection.commit()
+    print(cursor.rowcount,"records inserted")    
+    return jsonify({'message': 'Registration successful'}), 200
 
 
 
 @app.route('/android_login', methods=['POST'])
 def android_login():
-    return "Hi"
+    print('Login request from Android app received')
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+
+    cursor = connection.cursor()
+    cursor.execute("Select * from User where email = '{0}'".format(email))
+    results1 = cursor.fetchone()
+
+    if not results1:
+        return jsonify({'message':'User does not exist'}),500
+    
+    if username == results1['name'] and bcrypt.checkpw(password,results1['password']):
+        return jsonify({'message': 'Logged In successfully','username':results1['name']}),200
+    else:
+        return jsonify({'message': 'Invalid Credentials'}), 400
 
 
 
